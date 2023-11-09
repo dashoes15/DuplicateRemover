d5123b6e13d9bd1c243829712ef5a6147099836c
dashoes15 <daltonasali@gmail.com>
2023-11-09 09:43:12 -0500

    duplicate remover choice proj

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        String s;
+        String current;
+        String word = "";
+
+        System.out.println("Please enter a word or phrase:");
+        s = scan.nextLine();
+
+        for (int i  = 0 ; i < s.length(); i++) {
+            current = s.substring(i, i+1);
+            if (i == 0 || !s.substring(0, i).contains(current) ){
+                word += current;
+
+            }
+        }
+        System.out.println(word);
+
     }
 }
